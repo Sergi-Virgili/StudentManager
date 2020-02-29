451c4c9b31421fc4004f541bda6fa2860cfdd16a
Sergi Virgili <svirgilif@gmail.com>
2020-02-29 10:35:43 +0100

    Init environment configuration. close #1

---
+++ FileManager.DataAccess.Data/StudentDao.cs
@@ -0,0 +1,17 @@
+﻿using FileManager.Common.Layer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.DataAccess.Data
+{
+    public class StudentDao
+    {
+        public Student Add(Student student)
+        {
+            throw new NotImplementedException("The method is not implemented yet");
+        }
+    }
+}
