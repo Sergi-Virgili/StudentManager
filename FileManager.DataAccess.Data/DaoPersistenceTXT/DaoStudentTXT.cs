90b49478fd2b3a64aefdf08e86b4476d917b5980
Sergi Virgili <svirgilif@gmail.com>
2020-03-01 20:14:44 +0100

    DaoStuent class void interface implemented

--- FileManager.DataAccess.Data/DaoPersistenceTXT/DaoStudentTXT.cs
+++ FileManager.DataAccess.Data/DaoPersistenceTXT/DaoStudentTXT.cs
@@ -1,14 +1,15 @@
 ﻿using FileManager.Common.Layer;
 using System;
 using System.IO;
-
+using FileManager.DataAccess.Data.DaoInterfaces;
+using System.Collections.Generic;
 
 namespace FileManager.DataAccess.Data.DaoPersistenceTXT
 {
-    public class DaoStudentTXT
+    public class DaoStudentTXT : IStudentDAO
     {
         private static readonly String FileName = "./Students.txt";
-        public static Student Create(Student student)
+        public Student Create(Student student)
         {
 
             string line = student.Id.ToString() + ";" + student.Name + ";" + student.AgeOfBirth.ToString();
@@ -19,8 +20,23 @@ namespace FileManager.DataAccess.Data.DaoPersistenceTXT
 
             return student;
         }
+        
+        public Student Update (Student student)
+        {
+            throw new NotImplementedException("No implemented");
+             
+        }
 
-        public static void All()
+        public Boolean Delete (Student student)
+        {
+            throw new NotImplementedException("No implemented");
+        }
+
+        public Student Find (int id)
+        {
+            throw new NotImplementedException("No implemented");
+        }
+        public List<Student> All()
         {
 
             string[] lines = File.ReadAllLines(FileName);
@@ -30,6 +46,9 @@ namespace FileManager.DataAccess.Data.DaoPersistenceTXT
                 Console.WriteLine(values[0] + " " + values[1] + " " + values[2]);
 
             }
+
+            List<Student> result = new List<Student>();
+            return null;
 
         }
     }
