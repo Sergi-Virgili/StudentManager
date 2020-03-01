64dbdf3eca5eab94a187f7f79af443a021cffd16
Sergi Virgili <svirgilif@gmail.com>
2020-03-01 20:22:55 +0100

    DaoStudentXML.cs and DaoStudentJSON.cs Implements Concrete interface.

---
+++ FileManager.DataAccess.Data/DaoPersistenceJSON/DaoStudentJSON.cs
@@ -0,0 +1,55 @@
+﻿using FileManager.Common.Layer;
+using System;
+using System.IO;
+using FileManager.DataAccess.Data.DaoInterfaces;
+using System.Collections.Generic;
+
+namespace FileManager.DataAccess.Data.DaoPersistenceJSON
+{
+    public class DaoStudentJSON : IStudentDAO
+    {
+        private static readonly String FileName = "./Students.txt";
+        public Student Create(Student student)
+        {
+
+            string line = student.Id.ToString() + ";" + student.Name + ";" + student.AgeOfBirth.ToString();
+            using (StreamWriter file = new StreamWriter(FileName, true))
+            {
+                file.WriteLine(line);
+            }
+
+            return student;
+        }
+
+        public Student Update(Student student)
+        {
+            throw new NotImplementedException("No implemented");
+
+        }
+
+        public Boolean Delete(Student student)
+        {
+            throw new NotImplementedException("No implemented");
+        }
+
+        public Student Find(int id)
+        {
+            throw new NotImplementedException("No implemented");
+        }
+        public List<Student> All()
+        {
+
+            string[] lines = File.ReadAllLines(FileName);
+            foreach (var line in lines)
+            {
+                var values = line.Split(';');
+                Console.WriteLine(values[0] + " " + values[1] + " " + values[2]);
+
+            }
+
+            List<Student> result = new List<Student>();
+            return null;
+
+        }
+    }
+}
