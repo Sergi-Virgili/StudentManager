10c7a90e7cc22fdc800ce54adfc310a2a828b4af
Sergi Virgili <svirgilif@gmail.com>
2020-03-01 20:13:38 +0100

    Concrete Dao Persistence Interface

---
+++ FileManager.DataAccess.Data/DaoInterfaces/IStudentDAO.cs
@@ -0,0 +1,18 @@
+﻿using FileManager.Common.Layer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.DataAccess.Data.DaoInterfaces
+{
+    interface IStudentDAO
+    {
+        Student Create(Student student);
+        Student Update(Student student);
+        Boolean Delete(Student student);
+        Student Find(int id);
+        List<Student> All();
+    }
+}
