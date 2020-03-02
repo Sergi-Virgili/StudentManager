9f1626b9f929492d74c2082392b1e832378799ce
Sergi Virgili <svirgilif@gmail.com>
2020-03-02 13:19:29 +0100

    Add FactoryProvider

---
+++ FileManager.DataAccess.Data/FactoryProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.DataAccess.Data
+{
+    public class FactoryProvider
+    {
+        public static IAbstractFactory getFactory(string choice)
+        {
+            if (choice == "File") { return new FileFactory(); }
+            return null;
+        }
+    }
+}
