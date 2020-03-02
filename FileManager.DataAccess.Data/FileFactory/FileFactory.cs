f054752cceba21e399b83410b7d58f9a08f6c196
Sergi Virgili <svirgilif@gmail.com>
2020-03-02 13:20:51 +0100

    Add FileFactory

---
+++ FileManager.DataAccess.Data/FileFactory/FileFactory.cs
@@ -0,0 +1,35 @@
+﻿using FileManager.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.DataAccess.Data
+{
+    public class FileFactory : IAbstractFactory
+    {
+
+        public IFile Create(FileTypes fileFormat) {
+          
+            
+            switch (fileFormat)
+            {
+                case FileTypes.txt :
+                    Console.WriteLine("txt");
+                    return new FileTXT();
+                    
+                   
+                case FileTypes.json:
+                    Console.WriteLine("json");
+                    return new FileJSON();
+                  
+                case FileTypes.xml:
+                    return new FileXML();
+                default: return null;
+            }
+           
+        }
+      
+    }
+}
